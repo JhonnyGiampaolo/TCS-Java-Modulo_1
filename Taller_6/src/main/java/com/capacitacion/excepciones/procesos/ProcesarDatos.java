27dc383f4745559c157d8e73aee45333fb94a5e6
JhonnyGiampaolo <neogiampaolo@gmail.com>
2020-11-27 11:43:15 -0500

    Se suben Talleres 1 y 2 y se sube la estructura de los talleres 3,4,5 y 6

---
+++ Taller_6/src/main/java/com/capacitacion/excepciones/procesos/ProcesarDatos.java
@@ -0,0 +1,40 @@
+package com.capacitacion.excepciones.procesos;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ProcesarDatos {
+	
+	public int division(String numero1,String numero2) {
+		int resultado = 0;
+		
+		resultado=Integer.valueOf(numero1)/Integer.valueOf(numero2);
+		return resultado;
+		
+	}
+	
+	
+	public void arreglo(String[] lista) {
+		
+
+			for(int i=0;i<=5;i++) {
+				System.out.println(lista[i]);
+			}
+
+	}
+	
+	public void fecha(String input) {
+		
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+
+		try {
+			Date   date  = format.parse (input );
+			date.toString();
+		} catch (ParseException e) {
+
+		}
+	}
+	
+
+}
